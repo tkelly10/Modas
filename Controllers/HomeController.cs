86bb7524b3416432c354c3abe134529074a41eef
Tyler K <tkelly10@my.wctc.edu>
2020-02-03 13:05:12 -0600

    Adding assignment2

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,23 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Modas.Models;
+using Modas.Models.ViewModels;
 using System.Linq;
 
 namespace Modas.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly int PageSize = 10;
+        private readonly int PageSize = 20;
         private IEventRepository repository;
         public HomeController(IEventRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult Index(int page = 1) => View(
-            repository.Events.Include(e => e.Location)
-                .OrderBy(e => e.TimeStamp)
+        public ViewResult Index(int page = 1) => View(new EventListViewModel
+        {
+            Events = repository.Events
+                .Include(e => e.Location)
+                .OrderByDescending(e => e.TimeStamp)
                 .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+                .Take(PageSize),
+            PagingInfo = new PagingInfo
+            {
+                CurrentPage = page,
+                ItemsPerPage = PageSize,
+                TotalItems = repository.Events.Count()
+            }
+        });
     }
 }
---
+++ Models/ViewModels/EventListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Modas.Models.ViewModels
+{
+    public class EventListViewModel
+    {
+        public IEnumerable<Event> Events { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
